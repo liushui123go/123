86dd878c2548c8bf97646e0497caf01ffbc8c149
liushui123go <635181660@qq.com>
2016-07-28 17:11:03 +0800

    123

---
+++ gitTest/gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by lanou on 16/7/28.
+//  Copyright © 2016年 lbb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
